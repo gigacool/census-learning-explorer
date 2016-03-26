5c83ea6a87a55059b190c925b5422b48e364c93e
gigacool <cedrichartland@MacBook-Air-de-Cedric.local>
2016-03-26 22:26:03 +0100

    Add first version of API
    No unit tests there, some cleanup is required + database cleanup method
    is not plugged yet

---
+++ back-office/people.js
@@ -0,0 +1,73 @@
+var sqlite3 = require('sqlite3').verbose();
+// FIXME set db as an external parameter
+// TODO cleanup db to remove empty rows or skip them in queries
+
+var conversionMap = {
+  "varchar(1000)": "string",
+  "int": 'int'
+};
+
+module.exports = function(databasePath) {
+  var db = new sqlite3.Database(databasePath);
+  return {
+    // cleanup:function(callback){
+    //   db.serialize(function(){
+    //     db.each('SELECT count(age) FROM census_learn_sql WHERE age IS NULL;', function(err, row){
+    //       if (row['cound(age)'] !== 0){
+    //
+    //       }
+    //     }, function(){
+    //       callback();
+    //     });
+    //   });
+    // },
+    /* returns data meta data */
+    meta: function(request, response) {
+      var result = {
+        data: []
+      };
+      db.serialize(function() {
+        db.each("pragma table_info(census_learn_sql);", function(err, row) {
+          // here, we transform a bit the data toward a more applicative usage
+          result.data.push({
+            id: row.cid,
+            href: '/REST/people/' + encodeURIComponent(row.name),
+            name: row.name,
+            type: conversionMap[row.type]
+          });
+        }, function() {
+          response.send(result);
+        });
+      });
+    },
+    /* returns the applicative data for a given attribute*/
+    attribute: function(request, response) {
+      // FIXME is decodeURIComponent enough to prevent sql injection ?
+      var columnHeader = decodeURIComponent(request.params.attribute);
+      var map = {};
+      var result = {
+        attribute: columnHeader,
+        data: []
+      };
+      db.serialize(function() {
+        var query = "SELECT age,\"" + columnHeader + "\" FROM census_learn_sql;";
+        db.each(query, function(err, row) {
+          var localMap = map[row[columnHeader]];
+          if (!localMap) {
+            localMap = {
+              value: row[columnHeader],
+              ages: []
+            };
+            map[row[columnHeader]] = localMap;
+          }
+          localMap.ages.push(row.age);
+        }, function() {
+          for (key in map) {
+            result.data.push(map[key]);
+          }
+          response.send(result);
+        });
+      });
+    }
+  };
+};
---
+++ back-office/server.js
@@ -0,0 +1,40 @@
+/*
+ * the node server back-office is meant to be a REST API providing more or less raw access to database. First draft:
+ * /REST/people                                       --> return meta structure and links to available data-queries
+ * /REST/people/:attribute(?start=0&number=100)       --> return data relative to attribute (and age) with meta on
+ *                                                        remaining items
+ * Q? at this point, should the people/attribute query return raw lines or precalculated averages ? I think the data
+ * volume is too large if not using pagination, however, paginated data are incomplete data, how can I provide average on
+ * subset only. Espeically, I could add a bias in the SQL query (e.g. order by) that would cause data presented to be
+ * incorrect.
+ */
+var fs, databasePath, express, server, people;
+// Setup parameters
+console.info(process.argv.join(', '));
+if (process.argv.length > 2) {
+  databasePath = process.argv[2];
+}
+fs = require('fs');
+if (!fs.existsSync(databasePath)) {
+  console.error('The database path does not exist:', databasePath);
+  console.error('The server cannot start.');
+  process.exit(-1);
+}
+
+console.info('Starting database');
+people = require('./people')(databasePath);
+
+console.info('prepare server...');
+express = require('express');
+server = express();
+server.use('/', express.static('./front-office'));
+
+console.info('Getting API ready');
+server.get('/REST/people', people.meta);
+server.get('/REST/people/:attribute', people.attribute);
+
+server.listen(3000);
+console.info('Server ready at http://localhost:3000');
+
+// var open = require('open');
+// open('http://localhost:3000');
