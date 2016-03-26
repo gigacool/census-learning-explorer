ba1c28d5c5942d2bca3d68816ad6d929a7f7b627
gigacool <cedrichartland@MacBook-Air-de-Cedric.local>
2016-03-26 23:27:42 +0100

    fill up a first version of the attribute selector

--- front-office/sources/census/attributeSelection.js
+++ front-office/sources/census/attributeSelection.js
@@ -1,13 +1,77 @@
 window.census = window.census || {};
 
-window.census.attributeSelector = {
+AttributesCollection = Backbone.Collection.extend({
+  url: '/REST/people',
 
-  AttributesCollection:Backbone.Collection.extend({
-    url:'/REST/people',
+  parse: function(json) {
+    return json.data;
+  }
+});
 
-    parse:function(json){
-      return json.data;
+_AttributesView = Backbone.View.extend({
+  tagName: 'nav',
+  template: Handlebars.compile('{{#each attributes}}<a data-id="{{id}}">{{name}}</a>{{/each}}'),
+  intialize: function(options) {
+    this.container = options.container;
+    this.collection = options.collection;
+  },
+  events: {
+    'click a': 'selectItem'
+  },
+  selectItem: function(event) {
+    var data = this.collection.findWhere({
+      id: parseInt(event.target.dataset.id)
+    });
+    if (data) {
+      this.container.trigger('attribute-selected', data.toJSON());
     }
-  })
+  },
+  render: function(collection) {
+    this.$el.html({
+      attributes: collection.toJSON()
+    });
+    return this.$el;
+  }
+});
 
+AttributeSelectionView = Backbone.View.extend({
+  template: '<h2>Select an attribute</h2>' +
+    '<input type="text" name="attributes" placeholder="Find an attribute" value="">' +
+    '<div class="nav-container"></div>',
+  events: {
+    'keyup input': 'filterAttributes'
+  },
+
+  initialize: function(options) {
+    // NOP (?)
+  },
+
+  render: function() {
+    var that = this;
+    this.$el.html(this.template);
+    this.$nav = this.$el.find('.nav-container');
+    this.attributesListing = new _AttributesView({
+      container: this,
+      collection: this.collection
+    });
+    this.collection.fetch().done(function() {
+      that.$nav.html(that.attributesListing.render(that.collection));
+    }).fail(function() {
+      that.$el.html('<p class="error">Oups, something went wrong</p>');
+    });
+  },
+
+  filterAttributes: function(event) {
+    query = event.target.value;
+    filteredCollection = new AttributesCollection(this.collection.filter(function(model) {
+      return ~model.get('name').indexOf(query);
+    }));
+    that.$nav.html(that.attributesListing.render(that.collection));
+  }
+});
+
+
+window.census.attributeSelector = {
+  AttributesCollection: AttributesCollection,
+  AttributeSelectionView: AttributeSelectionView
 };
